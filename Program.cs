a6a746345c02f2a45dbc5f2f63fe812a17a017d0
CHANDRA BOSE <CHANDRA BOSE@LAPTOP-0D044MST>
2024-09-26 23:15:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace numberguesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+
+            while (playAgain)
+            {
+                int numberToGuess = random.Next(1, 101); 
+                int numberOfTries = 0;
+                bool numberGuessed = false;
+
+                Console.WriteLine("Welcome to the Guess Number Game!");
+                Console.WriteLine("You Will Be Asked To Guess A Number To Win The Game");
+                Console.WriteLine("---------------------------------------------------");
+
+                while (!numberGuessed)
+                {
+                    Console.Write("Enter a guess number between 1 to 100: ");
+                    int playerGuess = int.Parse(Console.ReadLine());
+                    numberOfTries++;
+
+                    if (playerGuess < numberToGuess)
+                    {
+                        Console.WriteLine("Oops! Sorry, too low.");
+                    }
+                    else if (playerGuess > numberToGuess)
+                    {
+                        Console.WriteLine("Oops! Sorry, too high.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Yayyy! Your Guessed Number is Correct.");
+                        Console.WriteLine($"You took {numberOfTries} tries! You Win the Game.");
+                        numberGuessed = true;
+                    }
+                }
+
+                
+                Console.Write("Do you want to play again? (y/n): ");
+                string response = Console.ReadLine().ToLower();
+                playAgain = response == "y";
+            }
+
+            Console.WriteLine("Thank you for playing! Goodbye!");
+        }
+    }
+    
+}
